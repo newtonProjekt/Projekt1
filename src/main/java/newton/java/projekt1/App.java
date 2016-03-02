1c5c8ec69d5a74ffc12a5e0be0263a0073f04d68
Johan Lindström <jolindse@hotmail.com>
2016-03-02 10:16:55 +0100

    Initial commit

---
+++ src/main/java/newton/java/projekt1/App.java
@@ -0,0 +1,13 @@
+package newton.java.projekt1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
